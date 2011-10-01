41f9b9917c59722f3d024a278db4146fd301468c
Gourab <gourab@circarconsilting.com>
2011-10-01 18:48:20 +0530

    modify on assets

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -2,7 +2,7 @@ class AssetsController < ApplicationController
    def show
     asset = Asset.find(params[:id])
     # do security check here
-    send_file asset.data.path, :type => asset.data_content_type, :x_sendfile => true
+    send_file asset.data.path, :type => asset.data_content_type
   end
 
    def destroy
