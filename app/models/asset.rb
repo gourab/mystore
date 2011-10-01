3e328a9a530bfec0124b79da8a68ad0636a2efd1
Gourab <gourab@circarconsilting.com>
2011-10-01 15:24:07 +0530

    modified on deploy file

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,8 +1,7 @@
 class Asset < ActiveRecord::Base
  has_attached_file  :data,
                     :url  => "/assets/:id",
-                  #  :path => ":rails_root/assets/docs/:id/:style/:basename.:extension"
-                  :path => ":rails_root/public/docs/:id/:style/:basename.:extension"
+                    :path => ":rails_root/assets/docs/:id/:style/:basename.:extension"
 
 
   belongs_to :attachable, :polymorphic => true
