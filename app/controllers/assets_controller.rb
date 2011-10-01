84d0273761bf89e6227264e745a3f965b6b794b9
Gourab <gourab@circarconsilting.com>
2011-10-01 17:54:31 +0530

    modify on deploy file

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,16 +1,18 @@
 class AssetsController < ApplicationController
-   def show
+  def show
     asset = Asset.find(params[:id])
     # do security check here
-    send_file asset.data.path, :type => asset.data_content_type
+    if asset && asset.data && asset.data.path
+      send_file asset.data.path, :type => asset.data_content_type
+    end
   end
 
-   def destroy
+  def destroy
     asset = Asset.find(params[:id])
     @asset_id = asset.id.to_s
     @allowed = Item::Max_Attachments - asset.attachable.assets.count
     asset.destroy
-     redirect_to :back
+    redirect_to :back
 
   end
 end
